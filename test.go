07580f637d775f834bf195bd371532448c3632bf
li772 <suweiwei0702@163.com>
2023-03-03 14:42:35 +0800

    testtttttt

--- test.go
+++ test.go
@@ -1,5 +1,13 @@
+/*
+ * @Author: libowei
+ * @Date: 2023-02-25 15:07:35
+ * @LastEditTime: 2023-03-03 14:42:02
+ * @FilePath: \godis\test.go
+ */
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("this is a test")
 }
